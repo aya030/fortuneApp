1d330cdad1b1f363788b8bf82f348fdb221a312c
ayn030 <kojinco18.033@gmail.com>
2022-04-14 00:01:51 +0900

    ＠Autowiredの削除

--- src/main/java/com/example/demo/controller/FortuneController.java
+++ src/main/java/com/example/demo/controller/FortuneController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
@@ -16,7 +15,6 @@ public class FortuneController {
 	// DI対象
 	private final FortuneService fortuneService;
 
-	@Autowired
 	public FortuneController(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
 	}
